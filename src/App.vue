c9b95dfde31180cb3c692fc40c14006baae19a67
Andrey <starkovvandrey@gmail.ru>
2022-05-21 07:44:26 +0700

    ver 2.0

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,80 @@
 <template>
     <input type="text" v-model="anime">
     <button v-on:click="Searchanime"> Hello </button>
-    <div class="icon">
-      <img id="ok"/>
-    </div>
-    <div class="information">
-      <h1 id="name"></h1>
-      <h2 id="date"></h2>
-      <h3 id="description"></h3>
-      <h4 id="rate"></h4>
-    </div>
+  <Animes v-for="anime of anime2" :key="anime.id"
+  v-bind:anime="anime"/>
+
+
 </template>
 
 <script>
 
-import {ref} from "vue";
+//import {ref} from "vue";
+import Animes from "@/components/Animes";
 
 export default {
   name: 'App',
   data(){
     return{
-      anime2: "null"
+      animelist: "null"
     }
   },
   components: {
+    Animes
   },
   props : ['anime','animelist2'],
-  methods:{
+  methods:
+      {
     async Searchanime(){
-      const animelist = ref([]);
+      //const animelist = ref([]);
       //console.log(`https://api.jikan.moe/v3/search/anime?q=${this.anime}`)
-          animelist.value= await fetch(`https://api.jikan.moe/v3/search/anime?q=${this.anime}`)
+          this.animelist= await fetch(`https://api.jikan.moe/v3/search/anime?q=${this.anime}`)
               .then(res => res.json()).then(data => data.results)
-      console.log(animelist.value[0].image_url);
+      //console.log(this.animelist.value[0].image_url);
+          console.log(this.animelist[0])
           //this.animelist2=animelist.value[0].image_url;
-          this.anime2=animelist.value[0];
+          //this.anime2=animelist.value[0];
+          //console.log(this.anime2)
       }
     },
-    watch: {
-       anime2(value) {
-          //value
-          document.getElementById("name").innerText=value.title
-         console.log(value)
-          document.getElementById("description").innerText=value.synopsis
-          document.getElementById("ok").src=value.image_url
-          document.getElementById("date").innerText = value.start_date[0] + value.start_date[1] + value.start_date[2] + value.start_date[3] + "-" + value.end_date[0]+value.end_date[1]+value.end_date[2]+value.end_date[3];
-          document.getElementById("rate").innerText = "Рейтинг: "+value.score
-          //document.getElementById("name")=value.
-        }
+    computed:{
+      // eslint-disable-next-line vue/return-in-computed-property
+      anime2(){
+        //const animelist = ref([]);
+        // eslint-disable-next-line vue/no-async-in-computed-properties
+        //animelist.value= fetch(`https://api.jikan.moe/v3/search/anime?q=${this.anime}`)
+        //    .then(res => res.json()).then(data => data.results)
+        //console.log(`https://api.jikan.moe/v3/search/anime?q=${this.anime}`)
+        // eslint-disable-next-line vue/no-async-in-computed-properties
+        //animelist.value = fetch(`https://api.jikan.moe/v3/search/anime?q=${this.anime}`)
+        //    .then(res => res.json()).then(data => data.results)
+        if (this.animelist!='null')
+        return this.animelist
+        else
+          return this.anime2
+      }
     }
+    //watch: {
+       //anime2(value) {
+
+          //value
+          //document.getElementById("name").innerText=value.title
+         //console.log(value)
+          //document.getElementById("description").innerText=value.synopsis
+          //document.getElementById("ok").src=value.image_url
+          //document.getElementById("date").innerText = value.start_date[0] + value.start_date[1] + value.start_date[2] + value.start_date[3] + "-" + value.end_date[0]+value.end_date[1]+value.end_date[2]+value.end_date[3];
+          //document.getElementById("rate").innerText = "Рейтинг: "+value.score
+          //document.getElementById("name")=value.
+         //const animelist = ref([]);
+         //console.log(`https://api.jikan.moe/v3/search/anime?q=${this.anime}`)
+         //animelist.value= fetch(`https://api.jikan.moe/v3/search/anime?q=${this.anime}`)
+             //.then(res => res.json()).then(data => data.results)
+         //console.log(animelist.value[0].image_url);
+         //this.animelist2=animelist.value[0].image_url;
+         // eslint-disable-next-line no-unused-vars
+         //value=animelist.value[0];
+       // }
+   // }
 
 }
 </script>
---
+++ src/components/Animes.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="icon">
+    <img id="ok" src="{{anime.image_url}}" />
+  </div>
+  <div class="information">
+    <h1 id="name">{{anime.title}}</h1>
+    <h2 id="date">{{anime.start_date[0]}}{{anime.start_date[1]}}{{anime.start_date[2]}}{{anime.start_date[3]}}</h2>
+    <h3 id="description">{{anime.synopsis}}</h3>
+    <h4 id="rate">{{anime.score}}</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Animes",
+  props: ["anime"]
+}
+</script>
+
+<style scoped>
+
+</style>
